fa2e6406a392083877b664c1a026ed8db319990e
Jaime Flores <opiatamus79@gmail.com>
2019-05-24 08:07:23 -0800

    Creating new Views and Models for employee.

---
+++ MVCApp/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employee = new Employee() { ID = 1, StaffID = 1, HRID = 1 };
+            return View(employee);
+        }
+    }
+}
