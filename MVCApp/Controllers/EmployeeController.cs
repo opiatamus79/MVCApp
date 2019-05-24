7125e297e9eaf85db4fb63024806e89dc1950a3d
Jaime Flores <opiatamus79@gmail.com>
2019-05-24 09:22:30 -0800

    Able to Create table with Employees data.

--- MVCApp/Controllers/EmployeeController.cs
+++ MVCApp/Controllers/EmployeeController.cs
@@ -10,10 +10,27 @@ namespace MVCApp.Controllers
     public class EmployeeController : Controller
     {
         // GET: Employee
+        public ActionResult GetEmployees()
+        {
+            //var employee = new Employee() { ID = 1, StaffID = 1, HRID = 1 };
+            return View();
+        }
+
+        // GET: Employees
         public ActionResult Index()
         {
-            var employee = new Employee() { ID = 1, StaffID = 1, HRID = 1 };
-            return View(employee);
+            var emp = new List<Employee>(); //Initializing emp for returning employees.
+            using (HRP_DBEntities3 databaseConn = new HRP_DBEntities3()) //NOTE CONNECTION STRING
+            {
+                var employees = databaseConn.Employees.OrderBy(a => a.FirstName).ToList();
+                emp = employees; //Json(new {employees= employees }, JsonRequestBehavior.AllowGet); // used to send json.
+            }
+
+
+
+
+
+            return View(emp);
         }
     }
 }
